2a10601b36f44b1511e5093832d527b0c3746045
Jamie Joung <jamiesjoung@gmail.com>
2022-09-14 01:18:12 -0600

    First Commit

---
+++ myApp/myAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myAppApp.swift
+//  myApp
+//
+//  Created by Jamie Joung on 9/12/22.
+//
+
+import SwiftUI
+
+@main
+struct myAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
